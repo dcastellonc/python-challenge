8515e2718511f0b1417aa48e101efa4a3c432a27
dcastellonc <danielacastellonc@outlook.com>
2023-01-16 15:33:41 -0500

    completed poll analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,10 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-
+#Creating array to store csv file data
 budget_data = []
 
+#Reading CSV
 with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -17,37 +18,41 @@ with open(csvpath) as csvfile:
         row = [row[0], int(row[1])]
         budget_data.append(row)
 
+#Determine total num of months
 months= len(budget_data)
+
+#Calculating net total of "Profit/Losses"
 net_total = sum([x[1] for x in budget_data])
 
+#Creating array to calculate monthly change in "Profit/Losses". Creating tuple to store date and "Profit/Losses".
 total_monthly_change = []
 for i in range(len(budget_data) - 1):
     total_monthly_change.append((budget_data[i+1][1] - budget_data[i][1], budget_data[i+1][0]))
 
+#Calculating average and rounded it to 2 decimal points
 total_monthly_average = round(sum([x[0] for x in total_monthly_change])/len([x[0] for x in total_monthly_change]),2)
 
-
-
-
+#Calculating greatest increase and decrease in profits 
 Increase = max(total_monthly_change)
 Decrease = min(total_monthly_change)
 
-
-print ("Financial Analysis")
-print("---------------------------")
+# Summary Table
+print (f"Financial Analysis")
+print(f"---------------------------")
 print (f"Total Months: {months}")
 print(f"Total: ${net_total}")
 print(f"Average Change: ${total_monthly_average}")
 print(f"Greatest Increase in Profits: {Increase [1]} (${Increase[0]})")
 print(f"Greatest Decrease in Profits: {Decrease[1]} (${Decrease[0]})")    
     
-output_file = os.path.join("analysis","analysis.txt")   
+#Text File    
+output_file = os.path.join("analysis","bank_analysis.txt")   
 with open(output_file, "w") as datafile:
    
-    datafile.write("Financial Analysis \n")
-    datafile.write("--------------------------- \n")
+    datafile.write(f"Financial Analysis \n")
+    datafile.write(f"--------------------------- \n")
     datafile.write(f"Total Months: {months}\n")
     datafile.write(f'Total: ${net_total}\n')
     datafile.write(f"Average Change: ${total_monthly_average}\n")
     datafile.write(f"Greatest Increase in Profits: {Increase [1]} (${Increase[0]})\n")
-    datafile.write(f"Greatest Decrease in Profits: {Decrease[1]} (${Decrease[0]})\n")
+    datafile.write(f"Greatest Decrease in Profits: {Decrease[1]} (${Decrease[0]})")
---
+++ PyPoll/main.py
@@ -0,0 +1,82 @@
+import os
+import csv
+
+
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+#Creating array to store csv file data
+election_results = []
+
+#Reading CSV
+with open(csvpath) as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvreader)
+    
+    for row in csvreader:
+        election_results.append(row)
+
+#Determine total votes
+total_votes = len(election_results)
+
+#Creating dictionary to count votes per candidate
+candidates = {}
+for x in election_results:
+    if x[2] in candidates:
+        candidates[x[2]] += 1
+        
+    else:
+        candidates[x[2]] = 1
+
+# Saving percentage of votes to dictionary
+for candidate, votes in candidates.items():
+    vote_percentage = round((votes/total_votes)*100,3)
+    candidates[candidate] = (votes,vote_percentage)
+
+#Separating info by candidate
+# Charles (1)
+candidate_1 = list(candidates)[0]   
+values_1 = list(candidates.values())[0] 
+votes_1 = (values_1[0])
+percentage_1 = (values_1[1])
+
+# Diana (2)
+candidate_2 = list(candidates)[1]   
+values_2 = list(candidates.values())[1] 
+votes_2 = (values_2[0])
+percentage_2 = (values_2[1])
+
+# Raymond (3)
+candidate_3 = list(candidates)[2]   
+values_3 = list(candidates.values())[2] 
+votes_3 = (values_3[0])
+percentage_3 = (values_3[1])
+
+
+# Summary Table
+print(f"Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"-------------------------")
+print(f"{candidate_1}: {percentage_1}% ({votes_1})")
+print(f"{candidate_2}: {percentage_2}% ({votes_2})")
+print(f"{candidate_3}: {percentage_3}% ({votes_3})")
+print(f"-------------------------")
+print(f"Winner: {candidate_2}")
+print(f"-------------------------")
+
+#Text File    
+output_file = os.path.join("analysis","election_results.txt")   
+with open(output_file, "w") as datafile:
+   
+    datafile.write(f"Election Results \n")
+    datafile.write(f"-------------------------\n")
+    datafile.write(f"Total Votes: {total_votes}\n")
+    datafile.write(f"-------------------------\n")
+    datafile.write(f"{candidate_1}: {percentage_1}% ({votes_1})\n")
+    datafile.write(f"{candidate_2}: {percentage_2}% ({votes_2})\n")
+    datafile.write(f"{candidate_3}: {percentage_3}% ({votes_3})\n")
+    datafile.write(f"-------------------------\n")
+    datafile.write(f"Winner: {candidate_2}\n")
+    datafile.write(f"-------------------------")
